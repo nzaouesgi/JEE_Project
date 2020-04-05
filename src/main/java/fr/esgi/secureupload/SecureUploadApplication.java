207ee7da64869268427b816b93b9064b4238a18b
nzaouesgi <hakhenaton@protonmail.com>
2020-04-05 17:25:58 +0200

    create spring app

---
+++ src/main/java/fr/esgi/secureupload/SecureUploadApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.secureupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureUploadApplication.class, args);
+    }
+
+}
