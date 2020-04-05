b3c8e971342d39fd30d6efaaf4afd8ab1fe3c635
nzaouesgi <hakhenaton@protonmail.com>
2020-04-05 17:25:58 +0200

    create spring app

---
+++ src/test/java/fr/esgi/secureupload/SecureUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.secureupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
