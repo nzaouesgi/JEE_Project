27cbb9b7d9ecebb429393a3586fddfd6550531e6
nzaouesgi <hakhenaton@protonmail.com>
2020-04-14 15:01:24 +0200

    refactor error handling

--- src/main/java/fr/esgi/secureupload/controllers/ApiExceptionHandler.java
+++ src/main/java/fr/esgi/secureupload/controllers/ApiExceptionHandler.java
@@ -17,7 +17,6 @@ import org.springframework.web.servlet.mvc.method.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,46 +25,46 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
+    public static Response.ErrorBody setStatusAndCreateErrorBody(Exception e, HttpServletResponse response, int status){
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+    }
+
     /* User API errors */
     @ExceptionHandler({User.PropertyValidationException.class})
     @ResponseBody
     public Response.ErrorBody handleUserPropertyValidation(User.PropertyValidationException e, HttpServletResponse response) {
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+        return setStatusAndCreateErrorBody(e, response, HttpStatus.BAD_REQUEST.value());
     }
 
     @ExceptionHandler({User.SecurityException.class})
     @ResponseBody
     public Response.ErrorBody handleUserSecurity(User.SecurityException e, HttpServletResponse response) {
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+        return setStatusAndCreateErrorBody(e, response, HttpStatus.FORBIDDEN.value());
     }
 
     @ExceptionHandler({User.MailAlreadyTakenException.class})
     @ResponseBody
     public Response.ErrorBody handlerUserMailAlreadyTaken(User.MailAlreadyTakenException e, HttpServletResponse response) {
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+        return setStatusAndCreateErrorBody(e, response, HttpStatus.FORBIDDEN.value());
     }
 
     @ExceptionHandler({User.PropertyNotFoundException.class})
     @ResponseBody
     public Response.ErrorBody handleUserPropertyNotFound(User.PropertyNotFoundException e, HttpServletResponse response) {
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+        return setStatusAndCreateErrorBody(e, response, HttpStatus.BAD_REQUEST.value());
     }
 
     @ExceptionHandler({User.NotFoundException.class})
     @ResponseBody
     public Response.ErrorBody handleUserNotFound(User.NotFoundException e, HttpServletResponse response) {
-        response.setStatus(HttpStatus.NOT_FOUND.value());
-        return new Response.ErrorBody(Collections.singletonList(e.getMessage()), response.getStatus());
+        return setStatusAndCreateErrorBody(e, response, HttpStatus.NOT_FOUND.value());
     }
 
     /* Generic errors */
     @ExceptionHandler({Exception.class})
     @ResponseBody
-    public Response.ErrorBody exceptionHandler(Exception e, HttpServletResponse response) {
+    public Response.ErrorBody handleException(Exception e, HttpServletResponse response) {
         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         this.logger.error(String.format("Unhandled exception: %s (%s).", e.getClass(), e.getMessage()));
         return new Response.ErrorBody(Collections.singletonList("An internal server error occurred."), response.getStatus());
@@ -73,12 +72,10 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request){
-
         List<String> errors = new ArrayList<>();
         for (FieldError error : e.getBindingResult().getFieldErrors()) {
             errors.add(error.getField() + ": " + error.getDefaultMessage());
         }
-
         return ResponseEntity.status(status).body(new Response.ErrorBody(errors, status.value()));
     }
 }
