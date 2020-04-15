1d82fc2636f1a61c0b72f41697bbb15317c8a9e5
nzaouesgi <hakhenaton@protonmail.com>
2020-04-15 13:15:04 +0200

    begin tests for user entity

--- src/main/java/fr/esgi/secureupload/SecureUploadApplication.java
+++ src/main/java/fr/esgi/secureupload/SecureUploadApplication.java
@@ -1,30 +1,12 @@
 package fr.esgi.secureupload;
-
-import fr.esgi.secureupload.entities.User;
-import fr.esgi.secureupload.services.UserService;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
 
 @SpringBootApplication
 public class SecureUploadApplication {
 
     public static void main(String[] args) {
-
-        ApplicationContext context = SpringApplication.run(SecureUploadApplication.class, args);
-
-
-        UserService service = context.getBean(UserService.class);
-
-        try {
-            User user = User.builder().email("nzaou.renaud@live.fr").password("password").build();
-            service.save(user);
-        } catch (Exception e){
-
-        }
-
+        SpringApplication.run(SecureUploadApplication.class, args);
     }
 
 }
--- src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
+++ src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -35,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder();
+        return new Argon2PasswordEncoder(32, 64, 4, 65535, 300);
     }
 
 }
