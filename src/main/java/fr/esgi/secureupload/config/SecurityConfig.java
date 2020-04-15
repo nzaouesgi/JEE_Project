e9775b19055298bb798154694f7d946ca42fe0ff
SystemGlitch <jeremy.la@outlook.fr>
2020-04-15 09:57:13 +0200

    Fix deploy job

--- src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
+++ src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import fr.esgi.secureupload.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,10 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().anyRequest().permitAll()
-                .and()
-                .formLogin();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin();
     }
 
     @Override
