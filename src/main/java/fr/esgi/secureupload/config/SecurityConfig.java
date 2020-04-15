2e5bbbb239dbefd727ef0018a58b8797395755bd
nzaouesgi <hakhenaton@protonmail.com>
2020-04-15 02:56:59 +0200

    access control improvements

--- src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
+++ src/main/java/fr/esgi/secureupload/config/SecurityConfig.java
@@ -23,7 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests().anyRequest().permitAll()
+                .and()
+                .formLogin();
     }
 
     @Override
