219de30331a7af41b66254d907cde7586a9c6234
nzaouesgi <hakhenaton@protonmail.com>
2020-06-23 20:30:30 +0200

    fixed mail server with mailhog

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-declare -a services=("database:3306" "smtp:25" "storage:10000" "storage:10001")
-
-for service in "${services[@]}"
+for service in "$@"
 do
    ./wait-for-it.sh -t 150 "$service"
 done
