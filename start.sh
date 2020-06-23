35686c28231635bf5f282bd87ac3e25228b70664
nzaouesgi <hakhenaton@protonmail.com>
2020-06-23 20:12:01 +0200

    added timeout for wait-for-it.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ declare -a services=("database:3306" "smtp:25" "storage:10000" "storage:10001")
 
 for service in "${services[@]}"
 do
-   ./wait-for-it.sh "$service"
+   ./wait-for-it.sh -t 150 "$service"
 done
 
 $*
